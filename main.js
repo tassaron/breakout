896a3b381f472711c50edd74d0dfd82a8119bef1
tassaron <rutachigal@gmail.com>
2022-02-24 19:52:08 -0500

    reformat with prettier

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-// A dirt simple breakout game based on this tutorial:
+// A dirt simple breakout game expanded from this tutorial:
 // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/
-// Made by brianna at tassaron.com
-const canvas = document.getElementById("game");
+// Made by Brianna Rainey <https://rainey.tech>
+const gamediv = document.getElementById("game");
+const canvas = document.createElement("canvas");
+gamediv.appendChild(canvas);
+canvas.width = gamediv.offsetWidth;
+canvas.height = gamediv.offsetHeight;
 const ctx = canvas.getContext("2d");
 const purple = "#993f70";
 let rightPressed = false;
@@ -16,7 +20,7 @@ let balls = [];
 
 // Load sprites, then start the game
 let sprites = new Image();
-sprites.addEventListener("load", function(){
+sprites.addEventListener("load", function () {
     canvas.addEventListener("touchstart", touchStartHandler, false);
     canvas.addEventListener("touchmove", touchMoveHandler, false);
     canvas.addEventListener("mousedown", mouseDownHandler, false);
@@ -24,23 +28,23 @@ sprites.addEventListener("load", function(){
     document.addEventListener("keydown", keyDownHandler, false);
     document.addEventListener("keyup", keyUpHandler, false);
     startGame();
-    draw()
-})
-sprites.src = "powerups.png";
+    draw();
+});
+sprites.src = "/static/client/rainey_arcade/js/breakout/powerups.png";
 
 // Timer for pausing the ball during countdowns
 // and giving invincibility frames after a death
-timer = {
+let timer = {
     ballPause: 0,
     diedRecently: 0,
-    dying: 0
+    dying: 0,
 };
 
 // Universal truths about a ball
-globalBall = {
+let globalBall = {
     speed: 4,
     radius: 12,
-    colour: "#000"
+    colour: "#000",
 };
 
 function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
@@ -80,12 +84,13 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
             return;
         }
 
-        if ( // ball collides with horizontal walls
+        if (
+            // ball collides with horizontal walls
             this.x + this.dx > canvas.width - globalBall.radius ||
             this.x + this.dx < globalBall.radius
         ) {
             // reverse ball direction and add some random angle
-            this.dx = -this.dx
+            this.dx = -this.dx;
             if (this.dx < 0) {
                 this.dx -= Math.random() * (globalBall.speed - 4);
             } else {
@@ -106,17 +111,23 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
                 if (this.dx < 0) {
                     var dir = 0;
                     if (this.dx <= -globalBall.speed) {
-                        this.dx = -globalBall.speed - Math.random() * ((globalBall.speed - 4) * 1);
+                        this.dx =
+                            -globalBall.speed -
+                            Math.random() * ((globalBall.speed - 4) * 1);
                     }
                 } else {
                     var dir = 1;
                     if (this.dx >= globalBall.speed) {
-                        this.dx = globalBall.speed + Math.random() * ((globalBall.speed - 4) * 1);
+                        this.dx =
+                            globalBall.speed +
+                            Math.random() * ((globalBall.speed - 4) * 1);
                     }
                 }
                 if (this.x - paddle.x < paddle.width / 2) {
                     // Ball hit left side of paddle
-                    let movement = Math.floor(-(this.x - (paddle.x + paddle.width / 2)) / 12);
+                    let movement = Math.floor(
+                        -(this.x - (paddle.x + paddle.width / 2)) / 12
+                    );
                     if (dir == 0) {
                         // Ball moving left
                         this.dx -= movement;
@@ -129,7 +140,9 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
                     }
                 } else {
                     // Ball hit right side of paddle
-                    let movement = Math.floor(-((paddle.x + paddle.width / 2) - this.x) / 12);
+                    let movement = Math.floor(
+                        -(paddle.x + paddle.width / 2 - this.x) / 12
+                    );
                     if (dir == 0) {
                         this.dx += movement;
                     } else {
@@ -156,7 +169,7 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
                 }
             }
         }
-    }
+    };
 
     this.collideWithBricks = function () {
         for (let col = 0; col < level.columnCount; col++) {
@@ -183,10 +196,13 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
                     this.dy = -this.dy;
                     if (
                         this.dx >= -globalBall.speed &&
-                        this.dx <= globalBall.speed) {
-                        this.dx = -this.dx + randomChoice(
-                            [0, 0, 0, 0, 0, 0, 1, 1, -1, -1, 2, -2]
-                        );
+                        this.dx <= globalBall.speed
+                    ) {
+                        this.dx =
+                            -this.dx +
+                            randomChoice([
+                                0, 0, 0, 0, 0, 0, 1, 1, -1, -1, 2, -2,
+                            ]);
                     } else {
                         this.dx = -this.dx;
                     }
@@ -199,31 +215,31 @@ function ball(i, x = canvas.width / 2, y = canvas.height - 64) {
                 }
             }
         }
-    }
+    };
 }
 balls[0] = new ball(0);
 
-paddle = {
+let paddle = {
     colour: purple,
     height: 16,
     width: 96,
     speed: 8,
     x: (canvas.width - this.width) / 2,
-    dir: 0
+    dir: 0,
 };
 
-level = {
+let level = {
     rowCount: 3,
     columnCount: 7,
     offsetTop: 64,
     offsetLeft: 32,
-    brickPadding: 8
+    brickPadding: 8,
 };
 
-defaultBrick = {
+let defaultBrick = {
     colour: purple,
     width: 76,
-    height: 24
+    height: 24,
 };
 
 function brick(x, y) {
@@ -238,27 +254,31 @@ function brick(x, y) {
 let powerup = {
     falling: 0,
     colour: [
-        "#000",    // nothing
+        "#000", // nothing
         "#008000", // grow paddle
         "#BA0707", // shrink paddle
         "#FFA500", // faster ball
-        "#800080",  // slower ball
-        "#3E3E3E" // multiball
+        "#800080", // slower ball
+        "#3E3E3E", // multiball
     ],
     width: 25, //defaultBrick.width / 3
     height: defaultBrick.height,
     x: 0,
     y: 0,
     dy: globalBall.speed,
-    dx: 0
+    dx: 0,
 };
 
 function createBricks() {
     for (let col = 0; col < level.columnCount; col++) {
         bricks[col] = [];
         for (let row = 0; row < level.rowCount; row++) {
-            let brickX = (col * (defaultBrick.width + level.brickPadding)) + level.offsetLeft;
-            let brickY = (row * (defaultBrick.height + level.brickPadding)) + level.offsetTop;
+            let brickX =
+                col * (defaultBrick.width + level.brickPadding) +
+                level.offsetLeft;
+            let brickY =
+                row * (defaultBrick.height + level.brickPadding) +
+                level.offsetTop;
             bricks[col][row] = new brick(brickX, brickY);
         }
     }
@@ -283,7 +303,7 @@ function resetBall() {
             "#fa482e",
             "#0a827c",
             "#f4a32e",
-        ])
+        ]);
     }
 
     // remove all balls but the first one, and replace it to center of screen
@@ -312,10 +332,7 @@ function randomChoice(arr) {
 }
 
 function spawnPowerup(newX, newY, ballId) {
-    if (
-        powerup.falling == 0 &&
-        Math.floor(Math.random() * 10) < 5
-    ) {
+    if (powerup.falling == 0 && Math.floor(Math.random() * 10) < 5) {
         powerup.x = newX;
         powerup.y = newY;
         powerup.dx = -balls[ballId].dx;
@@ -394,12 +411,17 @@ function keyUpHandler(e) {
 }
 
 /*
-* DRAWING FUNCTIONS
-*/
+ * DRAWING FUNCTIONS
+ */
 
 function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
+    ctx.rect(
+        paddle.x,
+        canvas.height - paddle.height,
+        paddle.width,
+        paddle.height
+    );
     ctx.fillStyle = paddle.colour;
     ctx.fill();
     ctx.closePath();
@@ -433,7 +455,17 @@ function drawPowerup() {
     ctx.fillStyle = powerup.colour[powerup.falling];
     ctx.fill();
     ctx.closePath();
-    ctx.drawImage(sprites, 25 * powerup.falling, 0, 25, 24, powerup.x, powerup.y, 25, 24);
+    ctx.drawImage(
+        sprites,
+        25 * powerup.falling,
+        0,
+        25,
+        24,
+        powerup.x,
+        powerup.y,
+        25,
+        24
+    );
 }
 
 function drawScore() {
@@ -445,15 +477,12 @@ function drawScore() {
 function drawLives() {
     ctx.font = "16pt Verdana";
     if (timer.diedRecently > 0) {
-        if (
-            timer.diedRecently % 15 == 0 &&
-            livesColour == "#000"
-        ) {
+        if (timer.diedRecently % 15 == 0 && livesColour == "#000") {
             livesColour = "#ff0000";
         } else if (
-            timer.diedRecently % 15 == 0 &&
-            livesColour == "#ff0000" ||
-            timer.diedRecently == 1) {
+            (timer.diedRecently % 15 == 0 && livesColour == "#ff0000") ||
+            timer.diedRecently == 1
+        ) {
             livesColour = "#000";
         }
         timer.diedRecently--;
@@ -472,7 +501,11 @@ function drawGameOver() {
     ctx.fillStyle = "#ff0000";
     ctx.fillText("Game Over", canvas.width / 2 - 132, canvas.height / 2 - 32);
     ctx.font = "16pt Verdana";
-    ctx.fillText("left-click to restart", canvas.width / 2 - 92, canvas.height / 2 + 22);
+    ctx.fillText(
+        "left-click to restart",
+        canvas.width / 2 - 92,
+        canvas.height / 2 + 22
+    );
     drawMuffin(ctx);
 }
 
@@ -554,7 +587,9 @@ function collidePowerupWithPaddle() {
                 let newBalls = [];
                 for (let i = 0; i < balls.length && balls.length < 17; i++) {
                     // create new ball for every ball that currently exists
-                    newBalls.push(new ball(balls.length + i, balls[i].x, balls[i].y));
+                    newBalls.push(
+                        new ball(balls.length + i, balls[i].x, balls[i].y)
+                    );
                 }
                 balls = balls.concat(newBalls);
                 break;
@@ -563,15 +598,11 @@ function collidePowerupWithPaddle() {
     }
 }
 
-
 function movePowerup() {
     powerup.x += powerup.dx;
     powerup.y += powerup.dy;
-    if (
-        powerup.x > canvas.width - powerup.width ||
-        powerup.x < 0
-    ) {
-        powerup.dx = -powerup.dx
+    if (powerup.x > canvas.width - powerup.width || powerup.x < 0) {
+        powerup.dx = -powerup.dx;
     }
     if (powerup.y == canvas.height - powerup.height) {
         powerup.falling = 0;
@@ -579,16 +610,9 @@ function movePowerup() {
 }
 
 function movePaddle() {
-    if (
-        rightPressed &&
-        paddle.x < canvas.width - (paddle.width / 2)
-    ) {
+    if (rightPressed && paddle.x < canvas.width - paddle.width / 2) {
         paddle.x += paddle.speed;
-    }
-    else if (
-        leftPressed &&
-        paddle.x > 0 - (paddle.width / 2)
-    ) {
+    } else if (leftPressed && paddle.x > 0 - paddle.width / 2) {
         paddle.x -= paddle.speed;
     }
 }
@@ -612,7 +636,12 @@ function drawMuffin(ctx) {
     ctx.save();
     ctx.translate(789.7776, 402.66194);
     ctx.save();
-    let g = ctx.createLinearGradient(431.43823, 309.14993, 455.10641, 306.89993);
+    let g = ctx.createLinearGradient(
+        431.43823,
+        309.14993,
+        455.10641,
+        306.89993
+    );
     g.addColorStop(0, "rgba(255, 92, 102, 1)");
     g.addColorStop(0.25, "rgba(255, 92, 102, 1)");
     g.addColorStop(0.5, "rgba(255, 77, 95, 1)");
@@ -662,8 +691,22 @@ function drawMuffin(ctx) {
     ctx.beginPath();
     ctx.moveTo(288.18821, 43.31544);
     ctx.lineTo(298.70770000000005, 169.54932);
-    ctx.bezierCurveTo(411.67574, 184.59742, 478.46090000000004, 170.90475, 529.22173, 150.79719);
-    ctx.bezierCurveTo(536.12483, 96.3052, 531.47084, -5.348219999999998, 524.19067, 28.679630000000003);
+    ctx.bezierCurveTo(
+        411.67574,
+        184.59742,
+        478.46090000000004,
+        170.90475,
+        529.22173,
+        150.79719
+    );
+    ctx.bezierCurveTo(
+        536.12483,
+        96.3052,
+        531.47084,
+        -5.348219999999998,
+        524.19067,
+        28.679630000000003
+    );
     ctx.closePath();
     ctx.fill("nonzero");
     ctx.stroke();
@@ -679,12 +722,54 @@ function drawMuffin(ctx) {
     ctx.miterLimit = "4";
     ctx.beginPath();
     ctx.moveTo(526.86343, -21.43929);
-    ctx.bezierCurveTo(543.19005, -48.707570000000004, 513.40583, -69.40107, 465.71123, -78.02789);
-    ctx.bezierCurveTo(190.31725999999998, -125.75193999999999, 231.11788, 108.26156999999999, 325.15244, 52.03463000000001);
-    ctx.bezierCurveTo(343.09062, 64.98277, 368.36647, 66.92441000000001, 400.90815, 57.96730000000001);
-    ctx.bezierCurveTo(416.24449, 65.65659000000001, 433.48974, 67.61916000000001, 452.47679, 64.35634);
-    ctx.bezierCurveTo(474.88674, 65.60239, 496.49178, 63.830040000000004, 514.08535, 47.01467);
-    ctx.bezierCurveTo(567.6820399999999, 42.80888, 556.14154, -3.9877599999999944, 526.86343, -21.439289999999993);
+    ctx.bezierCurveTo(
+        543.19005,
+        -48.707570000000004,
+        513.40583,
+        -69.40107,
+        465.71123,
+        -78.02789
+    );
+    ctx.bezierCurveTo(
+        190.31725999999998,
+        -125.75193999999999,
+        231.11788,
+        108.26156999999999,
+        325.15244,
+        52.03463000000001
+    );
+    ctx.bezierCurveTo(
+        343.09062,
+        64.98277,
+        368.36647,
+        66.92441000000001,
+        400.90815,
+        57.96730000000001
+    );
+    ctx.bezierCurveTo(
+        416.24449,
+        65.65659000000001,
+        433.48974,
+        67.61916000000001,
+        452.47679,
+        64.35634
+    );
+    ctx.bezierCurveTo(
+        474.88674,
+        65.60239,
+        496.49178,
+        63.830040000000004,
+        514.08535,
+        47.01467
+    );
+    ctx.bezierCurveTo(
+        567.6820399999999,
+        42.80888,
+        556.14154,
+        -3.9877599999999944,
+        526.86343,
+        -21.439289999999993
+    );
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -722,7 +807,14 @@ function drawMuffin(ctx) {
     ctx.miterLimit = "4";
     ctx.beginPath();
     ctx.moveTo(397.99094, 146.86723);
-    ctx.bezierCurveTo(409.15273, 138.63389, 428.84109, 136.55501, 443.0638, 145.90707);
+    ctx.bezierCurveTo(
+        409.15273,
+        138.63389,
+        428.84109,
+        136.55501,
+        443.0638,
+        145.90707
+    );
     ctx.fill();
     ctx.stroke();
     ctx.restore();
@@ -737,10 +829,38 @@ function drawMuffin(ctx) {
     ctx.transform(0.84606142, 0, 0, 1, 123.64927, -204);
     ctx.beginPath();
     ctx.moveTo(274.711, 302.51621220000004);
-    ctx.bezierCurveTo(279.59255366631373, 302.51621220000004, 283.5498348, 305.6820370890404, 283.5498348, 309.58728);
-    ctx.bezierCurveTo(283.5498348, 313.4925229109596, 279.59255366631373, 316.6583478, 274.711, 316.6583478);
-    ctx.bezierCurveTo(269.8294463336863, 316.6583478, 265.87216520000004, 313.4925229109596, 265.87216520000004, 309.58728);
-    ctx.bezierCurveTo(265.87216520000004, 305.6820370890404, 269.8294463336863, 302.51621220000004, 274.711, 302.51621220000004);
+    ctx.bezierCurveTo(
+        279.59255366631373,
+        302.51621220000004,
+        283.5498348,
+        305.6820370890404,
+        283.5498348,
+        309.58728
+    );
+    ctx.bezierCurveTo(
+        283.5498348,
+        313.4925229109596,
+        279.59255366631373,
+        316.6583478,
+        274.711,
+        316.6583478
+    );
+    ctx.bezierCurveTo(
+        269.8294463336863,
+        316.6583478,
+        265.87216520000004,
+        313.4925229109596,
+        265.87216520000004,
+        309.58728
+    );
+    ctx.bezierCurveTo(
+        265.87216520000004,
+        305.6820370890404,
+        269.8294463336863,
+        302.51621220000004,
+        274.711,
+        302.51621220000004
+    );
     ctx.closePath();
     ctx.fill("nonzero");
     ctx.stroke();
@@ -756,15 +876,43 @@ function drawMuffin(ctx) {
     ctx.transform(0.98343684, 0, 0, 1.0755093, 86.541108, -226.36949);
     ctx.beginPath();
     ctx.moveTo(394.91913, 297.5664491);
-    ctx.bezierCurveTo(398.23858634729015, 297.5664491, 400.9295374, 299.9408177891661, 400.9295374, 302.86975);
-    ctx.bezierCurveTo(400.9295374, 305.79868221083393, 398.23858634729015, 308.1730509, 394.91913, 308.1730509);
-    ctx.bezierCurveTo(391.59967365270984, 308.1730509, 388.9087226, 305.79868221083393, 388.9087226, 302.86975);
-    ctx.bezierCurveTo(388.9087226, 299.9408177891661, 391.59967365270984, 297.5664491, 394.91913, 297.5664491);
+    ctx.bezierCurveTo(
+        398.23858634729015,
+        297.5664491,
+        400.9295374,
+        299.9408177891661,
+        400.9295374,
+        302.86975
+    );
+    ctx.bezierCurveTo(
+        400.9295374,
+        305.79868221083393,
+        398.23858634729015,
+        308.1730509,
+        394.91913,
+        308.1730509
+    );
+    ctx.bezierCurveTo(
+        391.59967365270984,
+        308.1730509,
+        388.9087226,
+        305.79868221083393,
+        388.9087226,
+        302.86975
+    );
+    ctx.bezierCurveTo(
+        388.9087226,
+        299.9408177891661,
+        391.59967365270984,
+        297.5664491,
+        394.91913,
+        297.5664491
+    );
     ctx.closePath();
     ctx.fill("nonzero");
     ctx.stroke();
     ctx.restore();
     ctx.restore();
     ctx.restore();
-    ctx.moveTo(80000, 80000)
+    ctx.moveTo(80000, 80000);
 }
